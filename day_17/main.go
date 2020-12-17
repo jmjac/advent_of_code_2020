8e4229f52941e5fe3cecad4ec6ae8a38ad8ea6c3
jmjackowiak <jakub.m.jackowiak@gmail.com>
2020-12-17 17:00:57 +0100

    Replace the counting method

--- day_17/main.go
+++ day_17/main.go
@@ -32,36 +32,6 @@ func main() {
 	fmt.Printf("Part 2: %v\n", part1(data, 6, false))
 }
 
-func countNear(x, y, z, w int, ignoreW bool, grid map[Point]int) int {
-	count := 0
-	for pX := x - 1; pX <= x+1; pX++ {
-		for pY := y - 1; pY <= y+1; pY++ {
-			for pZ := z - 1; pZ <= z+1; pZ++ {
-				if ignoreW {
-					count += grid[Point{pX, pY, pZ, 0}]
-				} else {
-					for pW := w - 1; pW <= w+1; pW++ {
-						count += grid[Point{pX, pY, pZ, pW}]
-					}
-				}
-			}
-		}
-	}
-	return count - grid[Point{x, y, z, w}]
-}
-func changePoint(pX, pY, pZ, pW int, ignoreW bool, grid map[Point]int, newGrid map[Point]int) {
-	count := countNear(pX, pY, pZ, pW, ignoreW, grid)
-	if _, ok := grid[Point{pX, pY, pZ, pW}]; ok {
-		if count == 2 || count == 3 {
-			newGrid[Point{pX, pY, pZ, pW}] = 1
-		}
-	} else {
-		if count == 3 {
-			newGrid[Point{pX, pY, pZ, pW}] = 1
-		}
-	}
-}
-
 func part1(data []string, cycles int, ignoreW bool) int {
 	grid := make(map[Point]int)
 	for x, line := range data {
@@ -73,6 +43,7 @@ func part1(data []string, cycles int, ignoreW bool) int {
 	}
 	for i := 0; i < cycles; i++ {
 		newGrid := make(map[Point]int)
+		active := make(map[Point]int)
 		for p := range grid {
 			x, y, z, w := p.unpack()
 
@@ -80,15 +51,25 @@ func part1(data []string, cycles int, ignoreW bool) int {
 				for pY := y - 1; pY <= y+1; pY++ {
 					for pZ := z - 1; pZ <= z+1; pZ++ {
 						if ignoreW {
-							changePoint(pX, pY, pZ, 0, ignoreW, grid, newGrid)
+							active[Point{pX, pY, pZ, 0}]++
 						} else {
 							for pW := w - 1; pW <= w+1; pW++ {
-								changePoint(pX, pY, pZ, pW, ignoreW, grid, newGrid)
+								active[Point{pX, pY, pZ, pW}]++
 							}
 						}
 
 					}
 				}
+			}
+			active[Point{x, y, z, w}]--
+
+		}
+		for p, act := range active {
+			_, a := grid[p]
+			if a && (act == 2 || act == 3) {
+				newGrid[p] = 1
+			} else if act == 3 {
+				newGrid[p] = 1
 			}
 		}
 		grid = newGrid
