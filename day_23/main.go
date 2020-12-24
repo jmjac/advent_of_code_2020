24766330d86bb0bfdfc50088cd2e456e1e1ef1b0
jmjackowiak <jakub.m.jackowiak@gmail.com>
2020-12-24 02:50:10 +0100

    Solution day23 [go]

--- day_23/main.go
+++ day_23/main.go
@@ -6,12 +6,17 @@ import (
 	"log"
 )
 
-type node struct{
+type node struct {
 	value int
-	next *node
+	next  *node
 }
 
 func main() {
+	fmt.Printf("Part 1: %v\n,", part1(initializeInput()))
+	fmt.Printf("Part 2: %v\n", part2(initializeInput()))
+}
+
+func initializeInput() (*node, int, map[int]*node) {
 	data, err := ioutil.ReadFile("input.txt")
 
 	if err != nil {
@@ -20,11 +25,11 @@ func main() {
 
 	head := &node{}
 	current := head
-	nodesMap := make(map[int]*node)		
+	nodesMap := make(map[int]*node)
 
 	var length int
 	for _, i := range string(data) {
-		current.value = int(i-'0')
+		current.value = int(i - '0')
 		nodesMap[current.value] = current
 		current.next = &node{}
 		current = current.next
@@ -34,73 +39,74 @@ func main() {
 	current.next = head.next
 	head = current
 	nodesMap[head.value] = head
-	//fmt.Printf("Part 1: %v\n", part1(head, length, nodesMap))
-	fmt.Printf("Part 2: %v\n", part2(head, length, nodesMap))
+	return head, length, nodesMap
 }
 
-func part2(head *node, length int, nodesMap map[int]*node) int{
+func part2(head *node, length int, nodesMap map[int]*node) int {
 	current := head
-	for i:=10;i<1000001;i++ {
+	for i := 1; i < length; i++ {
+		current = current.next
+	}
+	current.next = &node{}
+	current = current.next
+	for i := length + 1; i <= 1000000; i++ {
 		current.value = i
-		current.next = &node{}
 		nodesMap[current.value] = current
+		current.next = &node{}
 		current = current.next
 		length++
 	}
 	current.value = head.value
 	current.next = head.next
 	head = current
-	for i:=0;i<10000000;i++{
-		current = move(current, length, nodesMap)
-		head = current
-	}
-	current = nodesMap[1]
-	fmt.Println(current.value)
-	fmt.Println(current.next.value)
+	nodesMap[head.value] = head
 
-	return current.next.value*current.next.next.value
+	for i := 0; i < 10000000; i++ {
+		head = move(head, length, nodesMap)
+	}
+
+	current = nodesMap[1].next
+	return current.value * current.next.value
 }
 
-func move(current *node, length int, nodesMap map[int]*node) *node{
+func move(current *node, length int, nodesMap map[int]*node) *node {
 	picked := current.next
+	current.next = picked.next.next.next
 	pickedMap := make(map[int]bool)
-	copyNode := current.next
-	for i:=0;i<3;i++{
+	copyNode := picked
+	for i := 0; i < 3; i++ {
 		pickedMap[copyNode.value] = true
 		copyNode = copyNode.next
 	}
-	//Pick the 3 values
-	current.next = current.next.next.next.next
 	putAt := current.value - 1
 	for {
-		if putAt <= 0{
-			putAt = length-1
+		if putAt == 0 {
+			putAt = length
 		}
 		_, alreadyPicked := pickedMap[putAt]
-		if alreadyPicked{
+		if alreadyPicked {
 			putAt--
-		}else{
+		} else {
 			break
 		}
 	}
 	currentNode := nodesMap[putAt]
-	nodesMap[putAt] = currentNode
 	picked.next.next.next = currentNode.next
 	currentNode.next = picked
 	return current.next
 }
 
 func part1(head *node, length int, nodesMap map[int]*node) string {
-	for i:=0;i<100;i++{
+	for i := 0; i < 100; i++ {
 		head = move(head, length, nodesMap)
 	}
-	for head.value!=1{
+	for head.value != 1 {
 		head = head.next
 	}
 	head = head.next
 
 	var ans string
-	for i:=0; i<length-1;i++{
+	for i := 0; i < length-1; i++ {
 		ans += fmt.Sprint(head.value)
 		head = head.next
 	}
